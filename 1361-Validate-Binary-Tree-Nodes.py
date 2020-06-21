f8f69812bd591b9201591774d2d08648eb9a5cf7
lnyan <lkwq007@gmail.com>
2020-06-21 10:45:26 +0800

    Update

--- 1361-Validate-Binary-Tree-Nodes.py
+++ 1361-Validate-Binary-Tree-Nodes.py
@@ -1,7 +1,11 @@
 from typing import List
 class Solution:
     def validateBinaryTreeNodes(self, n: int, leftChild: List[int], rightChild: List[int]) -> bool:
+        # simply using union find is not correct
+        # the testing cases might not cover all test cases
+        # we have to test the in degree 
         disjoint=[-1]*n
+        self.indegree=[0]*n
         self.total=n
         def search(idx):
             tmp=idx
@@ -26,11 +30,13 @@ class Solution:
             left=leftChild[idx]
             right=rightChild[idx]
             if left!=-1:
+                self.indegree[left]+=1
                 if union(idx,left):
                     self.total-=1
                 else:
                     return False
             if right!=-1:
+                self.indegree[right]+=1
                 if union(idx,right):
                     self.total-=1
                 else:
@@ -40,7 +46,32 @@ class Solution:
             if disjoint[idx]==-1:
                 if not dfs(idx):
                     return False
-        return self.total==1
-    
+        return all(map(lambda x:x==0 or x==1,self.indegree)) and sum(self.indegree)==n-1 and self.total==1
+
+class Solution:
+    def validateBinaryTreeNodes(self, n: int, leftChild: List[int], rightChild: List[int]) -> bool:
+        # the only one correct solution found in discussion
+        # https://leetcode.com/problems/validate-binary-tree-nodes/discuss/517557/C%2B%2B-Find-Root-%2B-DFS
+        indegree=[0]*n
+        for idx in range(n):
+            if leftChild[idx]!=-1:
+                indegree[leftChild[idx]]+=1
+            if rightChild[idx]!=-1:
+                indegree[rightChild[idx]]+=1
+            if indegree[leftChild[idx]]>1 or indegree[rightChild[idx]]>1:
+                return False
+        root=-1
+        for idx in range(n):
+            if indegree[idx]==0:
+                if root==-1:
+                    root=idx
+                else:
+                    return False
+        def dfs(idx):
+            if idx==-1:
+                return 0
+            return 1+dfs(leftChild[idx])+dfs(rightChild[idx])
+        return dfs(root)==n
+
 x=Solution()
-print(x.validateBinaryTreeNodes(n = 4, leftChild = [1,-1,3,-1], rightChild = [-1,3,-1,-1]))
+print(x.validateBinaryTreeNodes(n = 4, leftChild = [1,-1,3,-1], rightChild = [3,3,-1,-1]))
