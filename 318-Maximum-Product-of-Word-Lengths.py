f167cbbd19f4869e384cc417b4325c99c5ec7e0c
lnyan <lkwq007@gmail.com>
2020-07-14 10:05:19 +0800

    Update

--- 318-Maximum-Product-of-Word-Lengths.py
+++ 318-Maximum-Product-of-Word-Lengths.py
@@ -17,3 +17,29 @@ class Solution:
                 if word_masks[i]&word_masks[j]==0:
                     ret=max(ret,len(words[i])*len(words[j]))
         return ret
+
+class Solution:
+    def maxProduct(self, words: List[str]) -> int:
+        # pruning
+        mask=1
+        mapping={}
+        for i in range(ord("a"),ord("z")+1):
+            mapping[chr(i)]=mask
+            mask=mask<<1
+        def convert(word):
+            ret=0
+            for item in word:
+                ret|=mapping[item]
+            return ret
+        words.sort(key=lambda x:-len(x))
+        word_masks=[convert(item) for item in words]
+        ret=0
+        for i in range(len(words)):
+            if len(words[i])*len(words[i])<=ret: # the rest will not be counted
+                return ret
+            for j in range(i+1,len(words)):
+                if word_masks[i]&word_masks[j]==0:
+                    ret=max(ret,len(words[i])*len(words[j]))
+                    # the rest is less than ret
+                    break
+        return ret
