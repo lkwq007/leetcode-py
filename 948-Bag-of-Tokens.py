c93b0503983aac2064dc483ffa06f87ecd525a54
lnyan <lkwq007@gmail.com>
2020-09-06 13:19:24 +0800

    Update

--- 948-Bag-of-Tokens.py
+++ 948-Bag-of-Tokens.py
@@ -1,17 +1,22 @@
 class Solution:
     def bagOfTokensScore(self, tokens: List[int], P: int) -> int:
         if len(tokens)<2:
-            return 1 if tokens[0]<=P else 0
+            return 1 if len(tokens)!=0 and tokens[0]<=P else 0
         tokens.sort()
+        if tokens[0]>P:
+            return 0
         start=0
         end=len(tokens)-1
-        score=0
-        while start<=end:
-            while start<=end and tokens[start]<=P:
+        ret=0
+        while start<end and tokens[end]+P>tokens[start]:
+            while tokens[start]<=P:
+                ret+=1
                 P-=tokens[start]
                 start+=1
-                score+=1
-            if start>end:
-                return score
-            if tokens[end]
-        
+            if tokens[end]>tokens[start]:
+                P+=tokens[end]
+                ret-=1
+                end-=1
+        if start==end and P>=tokens[start]:
+            ret+=1
+        return ret
