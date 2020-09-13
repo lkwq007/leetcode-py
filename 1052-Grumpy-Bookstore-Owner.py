2cb7992a7f05f1c27188f06ffc2100a7fd71e399
lnyan <lkwq007@gmail.com>
2020-09-13 10:42:39 +0800

    Update

--- 1052-Grumpy-Bookstore-Owner.py
+++ 1052-Grumpy-Bookstore-Owner.py
@@ -9,11 +9,11 @@ class Solution:
                 acc+=customers[i]
         ret=acc
         for i in range(X,len(customers)):
+            if grumpy[i-X]==1:
+                acc-=customers[i-X]
             if grumpy[i]==0:
                 total+=customers[i]
             else:
                 acc+=customers[i]
-                if grumpy[i-X]==1:
-                    acc-=customers[i-X]
                 ret=max(ret,acc)
         return total+ret
