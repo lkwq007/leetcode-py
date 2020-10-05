b6c882f54c74afdad7fc1dd536b1da48a8eff37d
lnyan <lkwq007@gmail.com>
2020-10-05 13:53:31 +0800

    Update

--- 1288-Remove-Covered-Intervals.py
+++ 1288-Remove-Covered-Intervals.py
@@ -2,7 +2,14 @@ class Solution:
     def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
         if len(intervals)<2:
             return len(intervals)
-        intervals.sort(key=lambda x:(x[0],-x[1]))
+        intervals.sort()
         ret=[intervals[0]]
         for a,b in intervals[1:]:
             if ret[-1][0]<=a and b<=ret[-1][1]:
+                continue
+            elif a<=ret[-1][0] and ret[-1][1]<=b:
+                ret[-1][0]=a
+                ret[-1][1]=b
+            else:
+                ret.append([a,b])
+        return len(ret)
--- 45-Jump-Game-II.py
+++ 45-Jump-Game-II.py
@@ -2,7 +2,18 @@ class Solution:
     def jump(self, nums: List[int]) -> int:
         if len(nums)<2:
             return 0
-        target=set()
-        target.add(0)
-        jump=0
-        
+        # non-negative integers, we can always jump to last index
+        start=0
+        target=0
+        step=0
+        total=len(nums)
+        while target<total-1:
+            step+=1
+            max_end=target+1
+            for idx in range(start,target+1):
+                max_end=max(max_end,nums[idx]+idx)
+            if max_end>=total-1:
+                return step
+            target=max_end
+            start=target+1
+        return step
