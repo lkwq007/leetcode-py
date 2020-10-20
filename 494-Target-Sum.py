9a2e672d2dc69557cf8787648b81838ba238bc7a
lnyan <lkwq007@gmail.com>
2020-10-20 21:13:33 +0800

    Update

---
+++ 494-Target-Sum.py
@@ -0,0 +1,17 @@
+import functools
+class Solution:
+    def findTargetSumWays(self, nums: List[int], S: int) -> int:
+        nums.sort()
+        self.ret=0
+        @functools.lru_cache(maxsize=None)
+        def probe(i):
+            if i==len(nums)-1:
+                return {nums[i]:1,-nums[i]:1} if nums[i]!=0 else {nums[i]:2}
+            record={}
+            lst=probe(i+1)
+            for cur in (nums[i],-nums[i]):
+                for k,v in lst.items():
+                    record[cur+k]=record.get(cur+k,0)+v
+            return record
+        lst=probe(0)
+        return lst.get(S,0)
