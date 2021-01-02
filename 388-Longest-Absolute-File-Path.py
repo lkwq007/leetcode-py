08d36e400491148acf161b70c1be3a38e4f4f043
lnyan <lkwq007@gmail.com>
2021-01-02 12:22:27 +0800

    Update

--- 388-Longest-Absolute-File-Path.py
+++ 388-Longest-Absolute-File-Path.py
@@ -1,6 +1,25 @@
 class Solution:
     def lengthLongestPath(self, input: str) -> int:
         ret=0
+        stack=[(0,-1)]
+        lst=input.split("\n")
+        for item in lst:
+            start=0
+            while item[start]=="\t":
+                start+=1
+            name=item[start:]
+            while start<=stack[-1][1]:
+                stack.pop()
+            if "." in name:
+                ret=max(ret,stack[-1][0]+len(name))
+            else:
+                stack.append((stack[-1][0]+len(name)+1,start))
+        return ret
+
+
+class Solution:
+    def lengthLongestPath(self, input: str) -> int:
+        ret=0
         stack=[("",-1)]
         lst=input.split("\n")
         for item in lst:
@@ -10,17 +29,10 @@ class Solution:
             name=item[start:]
             while start<=stack[-1][1]:
                 stack.pop()
-            if start>stack[-1][1]:
-                if "." in name:
-                    ret=max(ret,len(stack[-1][0])+len(name))
-                else:
-                    stack.append((stack[-1][0]+name+"/",start))
+            if "." in name:
+                ret=max(ret,len(stack[-1][0])+len(name))
             else:
-
-                if "." in name:
-                    ret=max(ret,len(stack[-1][0])+len(name))
-                else:
-                    stack.append((stack[-1][0]+name+"/",start))
+                stack.append((stack[-1][0]+name+"/",start))
         return ret
 
 
