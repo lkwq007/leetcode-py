10535707494dd7405b877e63d48a763b21d1601a
lnyan <lkwq007@gmail.com>
2021-01-14 23:21:00 +0800

    Update

--- 1658-Minimum-Operations-to-Reduce-X-to-Z.py
+++ 1658-Minimum-Operations-to-Reduce-X-to-Z.py
@@ -2,17 +2,35 @@ class Solution:
     def minOperations(self, nums: List[int], x: int) -> int:
         left=0
         acc=0
+        # all numbers are postitive
+        total=len(nums)
         if sum(nums)<x:
             return -1
-        ret=len(nums)+1
-        right=len(nums)
-        while left<len(nums):
+        elif sum(nums)==x:
+            return total
+        left=0
+        ret=total+1
+        while left<total:
             acc+=nums[left]
             if acc==x:
-                total=len(nums)-right+left+1
-                ret=min(total,ret)
+                ret=left+1
+                break
             elif acc>x:
                 break
             left+=1
-        while right>=0:
-            
+        if left==total:
+            return -1
+        right=len(nums)-1
+        while right>left and left>=-1:
+            tmp=nums[right]+acc
+            if tmp>x and left>=0:
+                acc-=nums[left]
+                left-=1
+            elif tmp==x:
+                ret=min(ret,left+1+total-right)
+                acc=tmp
+                right-=1
+            else:
+                acc=tmp
+                right-=1
+        return ret if ret<total else -1
