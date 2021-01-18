f60b51a7bafcef22c9abdf7419cd59d4400a7dc1
lnyan <lkwq007@gmail.com>
2021-01-18 17:55:14 +0800

    Update

--- 1588-Sum-of-All-Odd-Length-Subarrays.py
+++ 1588-Sum-of-All-Odd-Length-Subarrays.py
@@ -1,3 +1,11 @@
 class Solution:
     def sumOddLengthSubarrays(self, arr: List[int]) -> int:
-        
+        total=len(arr)
+        ret=0
+        for i in range(total):
+            if i==0 or i==total-1:
+                ret+=(total+1)//2*arr[i]
+            else:
+                cnt=((i+2)//2)*((total-i+1)//2)+((i+1)//2)*((total-i)//2)
+                ret+=cnt*arr[i]
+        return ret
