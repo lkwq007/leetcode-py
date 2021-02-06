55193d8a21ca375d97969fefda99c4ee3377b3bb
lnyan <lkwq007@gmail.com>
2021-02-06 18:40:42 +0800

    Update

--- 1734-Decode-XORed-Permutation.py
+++ 1734-Decode-XORed-Permutation.py
@@ -4,20 +4,17 @@ class Solution:
         mid=len(encoded)//2
         acc=len(encoded)+1
         ret=[0]*acc
-        for i,item in enumerate(encoded,1):
-            acc^=i
-            if i<mid and i%2==0 or i>=mid and (len(encoded)-i)%2==0:
+        for i,item in enumerate(encoded,0):
+            acc^=(i+1)
+            if i<mid and i%2==0 or i>=mid and (len(encoded)-i-1)%2==0:
                 acc^=item
-        left=1
-        right=len(encoded)-2
-        print(acc)
         ret[mid]=acc
         for i in range(mid):
             ret[mid-i-1]=ret[mid-i]^encoded[mid-i-1]
             ret[mid+i+1]=ret[mid+i]^encoded[mid+i]
         return ret
 '''
-perm[1]
+perm[0]^perm[1]
 perm[1]^perm[2]
 perm[2]^perm[3]
 perm[3]^perm[4]
