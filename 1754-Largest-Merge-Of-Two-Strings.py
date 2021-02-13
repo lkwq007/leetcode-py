51bb4540e95240128c00308e2d7a509512f2f4ce
lnyan <lkwq007@gmail.com>
2021-02-13 17:52:43 +0800

    Update

--- 1754-Largest-Merge-Of-Two-Strings.py
+++ 1754-Largest-Merge-Of-Two-Strings.py
@@ -10,28 +10,10 @@ class Solution:
             elif word1[idx1]<word2[idx2]:
                 ret.append(word2[idx2])
                 idx2+=1
-            else:
-                i=idx1+1
-                j=idx2+1
-                flag=True
-                while i<len(word1) and j<len(word2):
-                    if word1[i]>word2[j]:
-                        break
-                    elif word1[i]<word2[j]:
-                        flag=False
-                        break
-                    i+=1
-                    j+=1
-                if i==len(word1):
-                    flag=False
-                if flag:
-                    while idx1<i:
-                        ret.append(word1[idx1])
-                        idx1+=1
-                else:
-                    while idx2<j:
-                        ret.append(word2[idx2])
-                        idx2+=1
+            elif word1[idx1:]>word2[idx2:]:
+                ret.append(word1[idx1])
+                idx1+=1
+
         idx,word=(idx1,word1) if idx1<len(word1) else (idx2,word2)
         while idx<len(word):
             ret.append(word[idx])
--- 712-Minimum-ASCII-Delete-Sum-for-Two-Str.py
+++ 712-Minimum-ASCII-Delete-Sum-for-Two-Str.py
@@ -1,13 +1,17 @@
 class Solution:
     def minimumDeleteSum(self, s1: str, s2: str) -> int:
-        template=[0]*(len(s2)+1)
-        dp=[template[:] for _ in range(len(s1)+1)]
+        # dp
+        lst1=[ord(item) for item in s1]
+        lst2=[ord(item) for item in s2]
+        total=sum(lst1)+sum(lst2)
+        template=[0]*(len(lst2)+1)
+        dp=[template[:] for _ in range(len(lst1)+1)]
         ret=0
-        for i in range(len(s1)):
-            for j in range(len(s2)):
-                if s1[i-1]==s2[j-1]:
-                    dp[i][j]=max(dp[i-1][j-1]+1,dp[i][j-1],dp[i-1][j])
+        for i in range(1,len(lst1)+1):
+            for j in range(1,len(lst2)+1):
+                if lst1[i-1]==lst2[j-1]:
+                    dp[i][j]=max(dp[i-1][j-1]+lst1[i-1]+lst2[j-1],dp[i-1][j],dp[i][j-1])
                 else:
-                    dp[i][j]=max(dp[i-1][j-1],dp[i][j-1],dp[i-1][j])
+                    dp[i][j]=max(dp[i-1][j-1],dp[i-1][j],dp[i][j-1])
                 ret=max(ret,dp[i][j])
-        return ret
+        return total-ret
