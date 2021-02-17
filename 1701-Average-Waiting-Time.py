56dc64ff2340bedab7b0f780d70f96555cd1d3f5
lnyan <lkwq007@gmail.com>
2021-02-17 10:56:42 +0800

    Update

--- 1701-Average-Waiting-Time.py
+++ 1701-Average-Waiting-Time.py
@@ -1,5 +1,11 @@
 class Solution:
     def averageWaitingTime(self, customers: List[List[int]]) -> float:
         acc=0
-        for arrival,time in customers:
-            
+        # non-decreasing
+        acc+=customers[0][1]
+        cur=sum(customers[0])
+        for i in range(1,len(customers)):
+            cur=max(cur,customers[i][0])
+            cur+=customers[i][1]
+            acc+=cur-customers[i][0]
+        return acc/len(customers)
