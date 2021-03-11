6dd127bd16303a055fd8ee8731477fe717842229
lnyan <lkwq007@gmail.com>
2021-03-11 16:20:40 +0800

    Update

--- 322-Coin-Change.py
+++ 322-Coin-Change.py
@@ -1,9 +1,14 @@
-import functools
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        coins.sort(reverse=True)
-        if amount<coins[-1]:
-            return -1
-        @functools.lru_cache(maxsize=None)
-        def local_change(amount,pos):
-            
+        dp=[-1]*(amount+1)
+        dp[0]=0
+        for val in coins:
+            dp[val]=1
+        for i in range(1,amount+1):
+            for val in coins:
+                if i-val>=0 and dp[i-val]!=-1:
+                    if dp[i]==-1:
+                        dp[i]=dp[i-val]+1
+                    else:
+                        dp[i]=min(dp[i],dp[i-val]+1)
+        return dp[amount]
