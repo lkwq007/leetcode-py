dc0f72b7642864875201dfcb59dcac00872e8d12
lnyan <lkwq007@gmail.com>
2021-03-26 15:24:17 +0800

    Update

--- 923-3Sum-With-Multiplicity.py
+++ 923-3Sum-With-Multiplicity.py
@@ -5,6 +5,30 @@ class Solution:
         ret=0
         for item in arr:
             lst[item]+=1
+        for i in range(len(lst)):
+            for j in range(i,len(lst)):
+                tmp=i+j
+                tmp=target-tmp
+                if 0<=tmp<=100 and tmp>=j:
+                    if i==j and j==tmp:
+                        acc=(lst[i]-2)*(lst[i]-1)*lst[i]//6
+                    elif i==j:
+                        acc=(lst[i]-1)*lst[i]*lst[tmp]//2
+                    elif j==tmp:
+                        acc=lst[i]*lst[j]*(lst[j]-1)//2
+                    else:
+                        acc=lst[i]*lst[j]*lst[tmp]
+                    ret+=acc
+                    ret%=term
+        return ret
+
+class Solution:
+    def threeSumMulti(self, arr: List[int], target: int) -> int:
+        term=10**9+7
+        lst=[0]*101
+        ret=0
+        for item in arr:
+            lst[item]+=1
         for i in range(len(arr)):
             lst[arr[i]]-=1
             new_lst=lst[:]
