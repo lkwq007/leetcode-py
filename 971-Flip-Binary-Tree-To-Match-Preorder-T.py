bda4cec8887d9b08757bf2af77345925d8bb6f8f
lnyan <lkwq007@gmail.com>
2021-03-29 19:23:46 +0800

    Update

--- 971-Flip-Binary-Tree-To-Match-Preorder-T.py
+++ 971-Flip-Binary-Tree-To-Match-Preorder-T.py
@@ -1,41 +1,40 @@
 # Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def flipMatchVoyage(self, root: TreeNode, voyage: List[int]) -> List[int]:
-        if root is None or len(voyage)<1:
-            return []
-        if root.val!=voyage[0]:
+        if root is None or len(voyage)<1 or root.val!=voyage[0]:
             return [-1]
         self.ret=[]
-        self.idx=1
+        self.idx=0
         def dfs(node):
+            if node.val==voyage[self.idx]:
+                self.idx+=1
+            else:
+                self.ret=[-1]
+                return
             if node.left and node.right:
-                if node.left.val==voyage[self.idx]:
-                    self.idx+=1
-                    dfs(node.left)
-                    dfs(node.right)
-                elif node.right.val==voyage[self.idx]:
-                    self.idx+=1
+                if node.right.val==voyage[self.idx]:
                     self.ret.append(node.val)
                     dfs(node.right)
                     dfs(node.left)
+                elif node.left.val==voyage[self.idx]:
+                    dfs(node.left)
+                    dfs(node.right)
                 else:
                     self.ret=[-1]
             elif node.left:
                 if node.left.val==voyage[self.idx]:
-                    self.idx+=1
                     dfs(node.left)
                 else:
                     self.ret=[-1]
             elif node.right:
                 if node.right.val==voyage[self.idx]:
-                    self.idx+=1
                     dfs(node.right)
                 else:
                     self.ret=[-1]
         dfs(root)
-        if self.idx
+        return self.ret if len(self.ret)==0 or self.ret[0]!=-1 else [-1]
