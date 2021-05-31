020da04e7e93ad69b384b0e615d70d0e11553b1d
lnyan <lkwq007@gmail.com>
2021-05-31 17:16:38 +0800

    Update

--- 956-Tallest-Billboard.py
+++ 956-Tallest-Billboard.py
@@ -1,17 +1,46 @@
+
 class Solution:
     def tallestBillboard(self, rods: List[int]) -> int:
-        dp=[[0 for _ in range(len(rods))] for _ in range(2501)]
-        rods.sort()
+        # TLE
         total=sum(rods)
-        ret=0
+        template=[False]*(len(rods)+1)
+        dp=[template[:] for _ in range(total*4+1)]
+        template=[0]*(len(rods)+1)
+        ret=[template[:] for _ in range(total*4+1)]
+        dp[total][0]=True
+        # dp[i][j] first i sum == j-total
         for i in range(len(rods)):
-            dp[rods[i]][i]+=1
-            for j in range(i):
-                for k in range(total):
-                    if dp[k][j]>0:
-                        dp[k+rods[i]][i]=max(dp[k+rods[i]][i],dp[k][j])
-        print(dp[100:])
-        return ret
+            for j in range(total*2+1):
+                dp[j][i+1]=dp[j][i] or dp[j+rods[i]][i] or dp[j-rods[i]][i]
+                if dp[j][i]:
+                    ret[j][i+1]=max(ret[j][i],ret[j][i+1])
+                if dp[j+rods[i]][i]:
+                    ret[j][i+1]=max(ret[j+rods[i]][i],ret[j][i+1])
+                if dp[j-rods[i]][i]:
+                    ret[j][i+1]=max(ret[j-rods[i]][i]+rods[i],ret[j][i+1])
+        return ret[total][-1]
+
+
+class Solution:
+    def tallestBillboard(self, rods: List[int]) -> int:
+        # TLE
+        total=sum(rods)
+        template=[False]*(total*4+1)
+        dp=[template[:] for _ in range(len(rods)+1)]
+        template=[0]*(total*4+1)
+        ret=[template[:] for _ in range(len(rods)+1)]
+        dp[0][total]=True
+        # dp[i][j] first i sum == j-total
+        for i in range(len(rods)):
+            for j in range(total*2+1):
+                dp[i+1][j]=dp[i][j] or dp[i][j+rods[i]] or dp[i][j-rods[i]]
+                if dp[i][j]:
+                    ret[i+1][j]=max(ret[i][j],ret[i+1][j])
+                if dp[i][j+rods[i]]:
+                    ret[i+1][j]=max(ret[i][j+rods[i]],ret[i+1][j])
+                if dp[i][j-rods[i]]:
+                    ret[i+1][j]=max(ret[i][j-rods[i]]+rods[i],ret[i+1][j])
+        return ret[-1][total]
 
 
 # class Solution:
