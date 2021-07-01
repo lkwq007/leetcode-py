efc2d57dc1214c055927d3c93fb52ba3455b61c5
lnyan <lkwq007@gmail.com>
2021-07-01 12:20:33 +0800

    Update

--- 1155-Number-of-Dice-Rolls-With-Target-Su.py
+++ 1155-Number-of-Dice-Rolls-With-Target-Su.py
@@ -2,9 +2,16 @@ class Solution:
     def numRollsToTarget(self, d: int, f: int, target: int) -> int:
         if target<d or d*f<target:
             return 0
-        dp=[0]*(target+1)
-        dp[d]=1
-        dp[d*f]=1
-        for idx in range(d+1,target):
-            
+        import functools
+        term=10**9+7
+        @functools.lru_cache(maxsize=None)
+        def probe(total,idx):
+            if idx==d:
+                return 1 if total==0 else 0
+            if total<=0:
+                return 0
+            ret=0
+            for i in range(1,f+1):
+                ret+=probe(total-i,idx+1)
+                ret%=term
         
