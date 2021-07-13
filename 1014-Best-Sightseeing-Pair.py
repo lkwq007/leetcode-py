1f0729175c1c454fd0a711212444bcca59cca9e7
lnyan <lkwq007@gmail.com>
2021-07-13 18:09:35 +0800

    Update

--- 1014-Best-Sightseeing-Pair.py
+++ 1014-Best-Sightseeing-Pair.py
@@ -1,7 +1,27 @@
 class Solution:
     def maxScoreSightseeingPair(self, A: List[int]) -> int:
+        ret=1
+        lst=[A[i]-i for i in range(len(A))]
+        acc=lst[-1]
+        for i in range(len(A)-2,-1,-1):
+            ret=max(A[i]+acc+i,ret)
+            acc=max(acc,lst[i])
+        return ret
+
+
+class Solution:
+    def maxScoreSightseeingPair(self, A: List[int]) -> int:
+        ret=1
+        acc=A[-1]-len(A)+1
+        for i in range(len(A)-2,-1,-1):
+            ret=max(A[i]+acc+i,ret)
+            acc=max(acc,A[i]-i)
+        return ret
+
+class Solution:
+    def maxScoreSightseeingPair(self, A: List[int]) -> int:
         ret=0
-        # brute force
+        # brute force, TLE
         for i in range(len(A)):
             for j in range(i+1,len(A)):
                 ret=max(A[j]+A[i]+i-j,ret)
---
+++ 1927-Sum-Game.py
@@ -0,0 +1,29 @@
+class Solution:
+    def sumGame(self, num: str) -> bool:
+        # even length
+        left=0
+        left_mark=0
+        right=0
+        right_mark=0
+        part=len(num)//2
+        for i in range(len(num)//2):
+            if num[i]=="?":
+                left_mark+=1
+            else:
+                left+=int(num[i])
+            if num[i+part]=="?":
+                right_mark+=1
+            else:
+                right+=int(num[i+part])
+        if (left_mark+right_mark)%2:
+            return True
+        elif (left_mark+right_mark)==0:
+            return left!=right
+        diff=left-right
+        # alice max diff
+        # bob min diff
+        lcnt=left_mark
+        rcnt=right_mark
+        step=0
+        for step in range(lcnt+rcnt):
+            
