f6680e675268887959f2cedf43439541ea24a031
lnyan <lkwq007@gmail.com>
2021-07-26 13:34:35 +0800

    Update

--- 935-Knight-Dialer.py
+++ 935-Knight-Dialer.py
@@ -2,5 +2,14 @@ class Solution:
     def knightDialer(self, N: int) -> int:
         if N<2:
             return 10
-        mapping=[[4,6],[6,8],[7,9],[4,8],[3,9],[],[1,7],[2,6],[1,3],[2,4]]
-        cnt=[]
+        term=10**9+7
+        mapping=[[4,6],[6,8],[7,9],[4,8],[3,9,0],[],[1,7,0],[2,6],[1,3],[2,4]]
+        count=[1]*10
+        for _ in range(1,N):
+            target=[0]*10
+            for i in range(10):
+                for next in mapping[i]:
+                    target[next]+=count[i]
+                    target[next]%=term
+            count=target
+        return sum(count)%term
