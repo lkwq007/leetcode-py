b5e01dfb2c3e86056ad3c3da39af172742b1ca94
lnyan <lkwq007@gmail.com>
2021-08-04 10:52:48 +0800

    Update

--- 90-Subsets-II.py
+++ 90-Subsets-II.py
@@ -12,3 +12,25 @@ class Solution:
             lst.pop()
         probe(0,[])
         return list(self.ret)
+
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        self.ret=[]
+        def probe(idx,lst):
+            if idx==len(nums):
+                self.ret.append(lst[:])
+                return
+            cnt=1
+            lst.append(nums[idx])
+            while idx+1<len(nums) and nums[idx+1]==nums[idx]:
+                lst.append(nums[idx])
+                idx+=1
+                cnt+=1
+            while cnt>0:
+                probe(idx+1,lst)
+                lst.pop()
+                cnt-=1
+            probe(idx+1,lst)
+        probe(0,[])
+        return self.ret
