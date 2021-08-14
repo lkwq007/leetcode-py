4e639e902dafde075432b29f383ff5c9b77a2424
lnyan <lkwq007@gmail.com>
2021-08-14 12:29:20 +0800

    Update

--- 1921-Eliminate-Maximum-Number-of-Monster.py
+++ 1921-Eliminate-Maximum-Number-of-Monster.py
@@ -1,3 +1,8 @@
 class Solution:
     def eliminateMaximum(self, dist: List[int], speed: List[int]) -> int:
-        
+        arrival=[x/y for x,y in zip(dist,speed)]
+        arrival.sort()
+        for i in range(len(dist)):
+            if arrival[i]<=i+1e-9:
+                return i
+        return len(dist)
