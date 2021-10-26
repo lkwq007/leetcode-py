4cfc1550d3b63977c1cf7db23fe63e63befddec1
lnyan <lkwq007@gmail.com>
2021-10-26 16:19:02 +0800

    Update

---
+++ 2048-Next-Greater-Numerically-Balanced-N.py
@@ -0,0 +1,4 @@
+class Solution:
+    def nextBeautifulNumber(self, n: int) -> int:
+        # brute force
+        # 0<=n<=10**6
--- 2049-Count-Nodes-With-the-Highest-Score.py
+++ 2049-Count-Nodes-With-the-Highest-Score.py
@@ -1,7 +1,10 @@
 class Solution:
     def countHighestScoreNodes(self, parents: List[int]) -> int:
+        if len(parents)<2:
+            return len(parents)-1
         left=[-1]*len(parents)
         right=[-1]*len(parents)
+        cnt=[0]*len(parents)
         total=len(parents)
         ret=0
         max_val=total-1
@@ -11,6 +14,46 @@ class Solution:
                 left[item]=i
             else:
                 right[item]=i
+        lst=[]
         for i in range(len(parents)):
             if left[i]==-1 and right[i]==-1:
-                
+                lst.append(i)
+        queue=lst
+        visited=[0]*len(parents)
+        while queue:
+            target=[]
+            for item in queue:
+                if visited[item]==1:
+                    continue
+                visited[item]=1
+                score=1
+                rest=total-1
+                cnt[item]=1
+                if left[item]!=-1:
+                    score*=cnt[left[item]]
+                    rest-=cnt[left[item]]
+                    cnt[item]+=cnt[left[item]]
+                if right[item]!=-1:
+                    score*=cnt[right[item]]
+                    rest-=cnt[right[item]]
+                    cnt[item]+=cnt[right[item]]
+                if rest>0:
+                    score*=rest
+                # print(item,score,max_val)
+                if score>max_val:
+                    ret=1
+                    max_val=score
+                elif score==max_val:
+                    ret+=1
+                if item!=0 and visited[parents[item]]==0:
+                    par=parents[item]
+                    if (left[par]!=-1 and visited[left[par]]!=1) or (right[par]!=-1 and visited[right[par]]!=1):
+                        pass
+                    else:
+                        target.append(par)
+            queue=target
+        return ret
+
+
+
+
