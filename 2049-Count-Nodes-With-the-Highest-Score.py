8934baeb2dd32dec1e922844cd6733cca4f358ab
lnyan <lkwq007@gmail.com>
2021-10-26 16:32:27 +0800

    Update

--- 2049-Count-Nodes-With-the-Highest-Score.py
+++ 2049-Count-Nodes-With-the-Highest-Score.py
@@ -57,3 +57,44 @@ class Solution:
 
 
 
+
+class Solution:
+    def countHighestScoreNodes(self, parents: List[int]) -> int:
+        stack=[(0,False)]
+        left=[-1]*len(parents)
+        right=[-1]*len(parents)
+        cnt=[0]*(len(parents)+1)
+        total=len(parents)
+        ret=0
+        max_val=total-1
+        for i in range(1,len(parents)):
+            item=parents[i]
+            if left[item]==-1:
+                left[item]=i
+            else:
+                right[item]=i
+        while stack:
+            node,flag=stack.pop()
+            leftp=left[node]
+            rightp=right[node]
+            if flag:
+                left_cnt=cnt[leftp]
+                right_cnt=cnt[rightp]
+                top=total-1-left_cnt-right_cnt
+                cnt[node]=left_cnt+right_cnt+1
+                score=1
+                for item in [left_cnt,right_cnt,top]:
+                    if item>0:
+                        score*=item
+                if score>max_val:
+                    max_val=score
+                    ret=1
+                elif score==max_val:
+                    ret+=1
+            else:
+                stack.append((node,True))
+                if rightp!=-1:
+                    stack.append((rightp,False))
+                if leftp!=-1:
+                    stack.append((leftp,False))
+        return ret
