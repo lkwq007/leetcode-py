ecca922d3cd8b99b9a0c93e7cffa243a0d0747e4
lnyan <lkwq007@gmail.com>
2021-10-28 11:18:10 +0800

    Update

--- 2033-Minimum-Operations-to-Make-a-Uni-Va.py
+++ 2033-Minimum-Operations-to-Make-a-Uni-Va.py
@@ -1,14 +1,33 @@
 class Solution:
     def minOperations(self, grid: List[List[int]], x: int) -> int:
         minval=grid[0][0]
-        maxval=grid[0][0]
         h=len(grid)
         w=len(grid[0])
-        for y in range(h):
-            for x in range(w):
-                item=grid[y][x]
-                maxval=max(maxval,item)
+        for i in range(h):
+            for j in range(w):
+                item=grid[i][j]
                 minval=min(minval,item)
-        if (maxval-minval)%x!=0:
-            return -1
-        
+        lst=[0]*10001
+        maxval=0
+        for i in range(h):
+            for j in range(w):
+                item=grid[i][j]-minval
+                if item%x!=0:
+                    return -1
+                lst[item//x]+=1
+                maxval=max(maxval,item//x)
+        left=0
+        right=maxval
+        ret=0
+        while left<right:
+            if lst[left]<lst[right]:
+                ret+=lst[left]
+                lst[left+1]+=lst[left]
+                lst[left]=0
+                left+=1
+            else:
+                ret+=lst[right]
+                lst[right-1]+=lst[right]
+                lst[right]=0
+                right-=1
+        return ret
