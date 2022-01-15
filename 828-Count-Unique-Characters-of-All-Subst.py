ffe579b257a7b4d20f5a6b010339d7d21c64e55a
lnyan <lkwq007@gmail.com>
2022-01-15 12:36:50 +0800

    Update

--- 828-Count-Unique-Characters-of-All-Subst.py
+++ 828-Count-Unique-Characters-of-All-Subst.py
@@ -1,14 +1,20 @@
 class Solution:
     def uniqueLetterString(self, s: str) -> int:
         left=[-1]*26
-        right=[-1]*26
+        last=[-1]*26
         ret=0
         for i in range(len(s)):
             item=ord(s[i])-ord("A")
-            ret+=(i-left[item])*last[item]
+            if last[item]!=-1:
+                ret+=(i-left[item])*last[item]
             last[item]=i-left[item]
             left[item]=i
+        cur=len(s)
+        for i in range(26):
+            if last[i]!=-1:
+                ret+=(cur-left[i])*last[i]
         return ret
+
 # class Solution:
 #     def uniqueLetterString(self, s: str) -> int:
 #         term=10**9+7
