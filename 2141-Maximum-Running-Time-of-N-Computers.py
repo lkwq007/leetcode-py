66d486844fa3f27c6b2a675539073d50b8d0e38c
lnyan <lkwq007@gmail.com>
2022-02-24 21:38:50 +0800

    Update

--- 2141-Maximum-Running-Time-of-N-Computers.py
+++ 2141-Maximum-Running-Time-of-N-Computers.py
@@ -2,16 +2,35 @@ class Solution:
     def maxRunTime(self, n: int, batteries: List[int]) -> int:
         if n==1:
             return sum(batteries)
+        if n==len(batteries):
+            return min(batteries)
         total=sum(batteries)
         batteries.sort(key=lambda x:-x)
         def check(x):
             if x*n>total:
                 return False
+            l=0
+            r=len(batteries)-1
+            racc=batteries[-1]
+            while l<n:
+                acc=batteries[l]
+                if acc>=x:
+                    l+=1
+                    continue
+                while acc+racc<x:
+                    r-=1
+                    if r<n:
+                        return False
+                    acc+=racc
+                    racc=batteries[r]
+                racc=max(0,acc+racc-x)
+                l+=1
             return True
         left=1
         right=total
         while left<right:
             middle=left+(right-left)//2
+            # print(middle,check(middle))
             if check(middle):
                 left=middle+1
             else:
--- 2167-Minimum-Time-to-Remove-All-Cars-Con.py
+++ 2167-Minimum-Time-to-Remove-All-Cars-Con.py
@@ -2,6 +2,7 @@ import functools
 class Solution:
     def minimumTime(self, s: str) -> int:
         # brute force
+        # 1<=len(s)<=2*10^5
         ret=len(s)
         left_lst=[0]*len(s)
         right_lst=[0]*len(s)
