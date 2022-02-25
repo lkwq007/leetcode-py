653aa9176b2f2e64751fbeb0a8eda6d2d744fdfe
lnyan <lkwq007@gmail.com>
2022-02-25 11:50:17 +0800

    Update

--- 2167-Minimum-Time-to-Remove-All-Cars-Con.py
+++ 2167-Minimum-Time-to-Remove-All-Cars-Con.py
@@ -4,5 +4,30 @@ class Solution:
         # brute force
         # 1<=len(s)<=2*10^5
         ret=len(s)
-        left_lst=[0]*len(s)
-        right_lst=[0]*len(s)
+        min_lst=[len(s)-1]*len(s)
+        min_idx=len(s)-1
+        cnt=0
+        for item in s:
+            if item=="1":
+                cnt+=1
+        # first remove from middle
+        right=[0]*(len(s)+1)
+        last=len(s)
+        lacc=0
+        racc=0
+        for i in range(len(s)-1,-1,-1):
+            right[i]=(cnt-racc)*2+(len(s)-last)
+            if right[i]<right[min_idx]:
+                min_idx=i
+            min_lst[i]=min_idx
+            if s[i]=="1":
+                last=i
+                racc+=1
+        last=-1
+        for i in range(len(s)):
+            idx=min_lst[i]
+            ret=min(ret,right[idx]-2*lacc+last+1)
+            if s[i]=="1":
+                lacc+=1
+                last=i
+        return ret
