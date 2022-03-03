a1d9c3cb6ad3eb43324ede2833a1f6c429a44c78
lnyan <lkwq007@gmail.com>
2022-03-03 13:38:58 +0800

    Update

--- 2063-Vowels-of-All-Substrings.py
+++ 2063-Vowels-of-All-Substrings.py
@@ -1,11 +1,41 @@
 class Solution:
     def countVowels(self, word: str) -> int:
-        ret=0
-        cur=0
+        # brute force with optim
+        cnt=0
         acc=0
+        ret=0
+        base=0
+        stack=0
         for item in word:
             if item in "aeiou":
-                cur+=1
-            acc+=cur
-            ret+=acc
+                cnt+=1
+                acc+=1
+                base+=cnt
+                stack+=base
+                cnt=0
+            else:
+                cnt+=1
+            ret+=stack
+        return ret
+
+
+class Solution:
+    def countVowels(self, word: str) -> int:
+        # TLE
+        # brute force, no optim, TLE
+        cnt=0
+        acc=0
+        ret=0
+        stack=[]
+        for item in word:
+            if item in "aeiou":
+                cnt+=1
+                acc+=1
+                stack.append(cnt)
+                cnt=0
+            else:
+                cnt+=1
+            for i in range(len(stack)):
+                idx=i+1
+                ret+=stack[-idx]*idx
         return ret
