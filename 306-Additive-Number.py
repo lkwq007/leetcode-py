5e4961528788cfb63af80bf07ca37880b9859539
lnyan <lkwq007@gmail.com>
2022-03-04 10:49:57 +0800

    Update

--- 306-Additive-Number.py
+++ 306-Additive-Number.py
@@ -1,3 +1,22 @@
 class Solution:
     def isAdditiveNumber(self, num: str) -> bool:
-        def probe()
+        def probe(pos,last,total):
+            for i in range(pos,len(num)):
+                cur=int(num[pos:i+1])
+                if cur==total and (i+1==len(num) or probe(i+1,total,last+total)):
+                    return True
+                if num[pos]=="0":
+                    break
+            return False
+        for i in range(len(num)):
+            for j in range(i+1,len(num)):
+                first=int(num[:i+1])
+                second=int(num[i+1:j+1])
+                if probe(j+1,second,first+second):
+                    return True
+                if num[i+1]=="0":
+                    break
+            if num[0]=="0":
+                break
+        return False
+                
