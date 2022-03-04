8b511a8a9f8906afbb6bb698c262d67922800819
lnyan <lkwq007@gmail.com>
2022-03-04 10:35:14 +0800

    Update

--- 2122-Recover-the-Original-Array.py
+++ 2122-Recover-the-Original-Array.py
@@ -1,12 +1,26 @@
-import re
-
-
 class Solution:
     def recoverArray(self, nums: List[int]) -> List[int]:
         # sum(nums)=2*sum(arr)
         # N^2?
+        # possible k?
+        nums.sort()
+        n=len(nums)//2
         record={}
         for item in nums:
             record[item]=record.get(item,0)+1
-        nums.sort()
-        
+        for i in range(n):
+            k=nums[i+1]-nums[0]
+            if k%2 or k==0:
+                continue
+            cnt=record.copy()
+            ret=[]
+            for j in range(len(nums)):
+                item=nums[j]
+                if cnt.get(item,0)>0:
+                    cnt[item]-=1
+                    if cnt.get(item+k,0)==0:
+                        break
+                    ret.append(item+k//2)
+                    cnt[item+k]-=1
+            if len(ret)==n:
+                return ret
