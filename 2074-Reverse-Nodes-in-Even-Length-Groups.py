b935eefd9cf88e8cf113ed80ad5897e5427d46f4
lnyan <lkwq007@gmail.com>
2022-03-08 10:22:38 +0800

    Update

--- 2074-Reverse-Nodes-in-Even-Length-Groups.py
+++ 2074-Reverse-Nodes-in-Even-Length-Groups.py
@@ -1,40 +1,33 @@
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def reverseEvenLengthGroups(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # two pass
-        group=1
+        last=head
+        ptr=head.next
+        target=2
         cnt=0
-        ptr=head
-        last=None
-        prev=None
         while ptr:
             cnt+=1
-            if group&1:
-                last=ptr
-                ptr=ptr.next
-                if cnt==group:
-                    group+=1
-                    cnt=0
-            else:
-                next=ptr.next
-                if cnt==1:
-                    tail=ptr
-                else:
+            if cnt==target or ptr.next is None:
+                if cnt%2==0:
+                    next_group=ptr.next
+                    ptr=last.next
+                    prev=next_group
+                    while ptr.next!=next_group:
+                        next=ptr.next
+                        ptr.next=prev
+                        prev=ptr
+                        ptr=next
                     ptr.next=prev
-                prev=ptr
-                ptr=next
-                if cnt==group:
-                    tail.next=ptr
-                    last.next=prev
-                    cnt=0
-                    group+=1
-                if ptr==None:
-                    if cnt&1:
-                        tmp=prev
-                        prev=None
+                    tmp=ptr
+                    ptr=last.next
+                    last.next=tmp
+                last=ptr
+                target+=1
+                cnt=0
+            ptr=ptr.next
         return head
         
