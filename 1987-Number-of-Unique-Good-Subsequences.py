6d40b8eff0df4e4b0070545c5e6c222e2ef740cf
lnyan <lkwq007@gmail.com>
2022-03-09 09:54:50 +0800

    Update

--- 1987-Number-of-Unique-Good-Subsequences.py
+++ 1987-Number-of-Unique-Good-Subsequences.py
@@ -1,4 +1,17 @@
 class Solution:
     def numberOfUniqueGoodSubsequences(self, binary: str) -> int:
         term=10**9+7
-        
+        zero=0
+        one=0
+        extra=0
+        if "0" in binary:
+            extra=1
+        for i in range(len(binary)):
+            item=binary[i]
+            if item=="0":
+                zero=zero+one
+                zero%=term
+            else:
+                one=one+zero+1
+                one%=term
+        return (zero+one+extra)%term
