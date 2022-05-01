394366bffde6399bf66951d783a25296435e89f9
lnyan <lkwq007@gmail.com>
2022-05-01 11:58:09 +0800

    Update

--- 1937-Maximum-Number-of-Points-with-Cost.py
+++ 1937-Maximum-Number-of-Points-with-Cost.py
@@ -1,26 +1,20 @@
 class Solution:
     def maxPoints(self, points: List[List[int]]) -> int:
-        # brute force, TLE
         dp=points[0][:]
         ret=max(dp)
         h=len(points)
         w=len(points[0])
-        target=[0]*w
         for y in range(1,h):
-            max_idx=-1
+            target=[0]*w
+            lst=[0]*(w+1)
+            for x in range(w-1,-1,-1):
+                lst[x]=max(dp[x],lst[x+1]-1)
+            lacc=0
             for x in range(w):
-                target[x]=-w
-                if max_idx>=x:
-                    pass
-                else:
-                    max_idx=0
-                    for i in range(w):
-                        if dp[i]-abs(i-x)>=dp[max_idx]-abs(max_idx-x):
-                            max_idx=i
-                target[x]=points[y][x]+dp[max_idx]-abs(max_idx-x)
-                ret=max(ret,target[x])
-            dp,target=target,dp
-        return ret
+                target[x]=points[y][x]+max(lacc,lst[x])
+                lacc=max(lacc-1,dp[x]-1)
+            dp=target
+        return max(dp)
 
 class Solution:
     def maxPoints(self, points: List[List[int]]) -> int:
--- 844-Backspace-String-Compare.py
+++ 844-Backspace-String-Compare.py
@@ -2,12 +2,34 @@ class Solution:
     def backspaceCompare(self, S: str, T: str) -> bool:
         # constant space
         idx1=len(S)-1
-        skip1=0
         idx2=len(T)-1
+        skip1=0
         skip2=0
         while idx1>=0 or idx2>=0:
-            if S[idx1]=="#":
-                skip1
+            if (idx1>=0 and S[idx1]=="#") or (idx2>=0 and T[idx2]=="#"):
+                while idx1>=0 and S[idx1]=="#":
+                    skip1+=1
+                    idx1-=1
+                while idx2>=0 and T[idx2]=="#":
+                    skip2+=1
+                    idx2-=1
+            elif skip1>0 or skip2>0:
+                while idx1>=0 and skip1>0 and S[idx1]!="#":
+                    skip1-=1
+                    idx1-=1
+                if idx1<0:
+                    skip1=0
+                while idx2>=0 and skip2>0 and T[idx2]!="#":
+                    skip2-=1
+                    idx2-=1
+                if idx2<0:
+                    skip2=0
+            else:
+                if idx1<0 or idx2<0 or S[idx1]!=T[idx2]:
+                    return False
+                idx1-=1
+                idx2-=1
+        return True
 
 class Solution:
     def backspaceCompare(self, S: str, T: str) -> bool:
