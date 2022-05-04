db227c268308ac6a02674b4fcffadc90847517b7
lnyan <lkwq007@gmail.com>
2022-05-04 17:05:39 +0800

    Update

--- 2062-Count-Vowel-Substrings-of-a-String.py
+++ 2062-Count-Vowel-Substrings-of-a-String.py
@@ -21,3 +21,25 @@ class Solution:
                 if acc&full==full:
                     ret+=1
         return ret
+
+
+class Solution:
+    def countVowelSubstrings(self, word: str) -> int:
+        record={}
+        start={}
+        lst=set(list("aeiou"))
+        ret=0
+        last=0
+        for i,item in enumerate(word):
+            if item in lst:
+                record[item]=i
+                if item not in start:
+                    start[item]=i
+                if len(start)==5:
+                    last=min(record.values())-min(start.values())+1
+                ret+=last
+            else:
+                record={}
+                start={}
+                last=0
+        return ret
--- 2257-Count-Unguarded-Cells-in-the-Grid.py
+++ 2257-Count-Unguarded-Cells-in-the-Grid.py
@@ -1,3 +1,31 @@
 class Solution:
     def countUnguarded(self, m: int, n: int, guards: List[List[int]], walls: List[List[int]]) -> int:
-        
+        template=[0]*n
+        grid=[template[:] for _ in range(m)]
+        for y,x in walls:
+            grid[y][x]=3
+        for y,x in guards:
+            grid[y][x]=3
+        queue=[(y,x,0) for y,x in guards]+[(y,x,1) for y,x in guards]
+        while queue:
+            target=[]
+            for y,x,t in queue:
+                if t==0:
+                    xn=x
+                    for yn in [y-1,y+1]:
+                        if 0<=xn<n and 0<=yn<m and grid[yn][xn]&1==0:
+                            grid[yn][xn]=grid[yn][xn]|1
+                            target.append((yn,xn,t))
+                else:
+                    yn=y
+                    for xn in [x-1,x+1]:
+                        if 0<=xn<n and 0<=yn<m and grid[yn][xn]&2==0:
+                            grid[yn][xn]=grid[yn][xn]|2
+                            target.append((yn,xn,t))
+            queue=target
+        ret=0
+        for y in range(m):
+            for x in range(n):
+                if grid[y][x]==0:
+                    ret+=1
+        return ret
