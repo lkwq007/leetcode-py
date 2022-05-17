b6550a644d8b590c548d94341971b76aa5684760
lnyan <lkwq007@gmail.com>
2022-05-17 16:14:18 +0800

    Update

--- 1248-Count-Number-of-Nice-Subarrays.py
+++ 1248-Count-Number-of-Nice-Subarrays.py
@@ -1,6 +1,43 @@
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
+        # 2 pointer also work
+        record={0:1}
+        cnt=0
         ret=0
-        i=0
-        last=-1
-        
+        for i in range(len(nums)):
+            item=nums[i]
+            if item%2:
+                cnt+=1
+            ret+=record.get(cnt-k,0)
+            record[cnt]=record.get(cnt,0)+1
+        return ret
+class Solution:
+    def numberOfSubarrays(self, nums: List[int], k: int) -> int:
+        left=-1
+        right=-1
+        ret=0
+        cnt=0
+        idx=0
+        while idx<len(nums):
+            if nums[idx]%2:
+                cnt+=1
+                right=idx
+            idx+=1
+            if cnt==k:
+                break
+        if idx==len(nums):
+            return 0
+        while idx<len(nums):
+            if nums[idx]%2:
+                lacc=1
+                while left+lacc<len(nums) and nums[left+lacc]%2==0:
+                    lacc+=1
+                ret+=lacc*(idx-right)
+                left=left+lacc
+                right=idx
+            idx+=1
+        lacc=1
+        while left+lacc<len(nums) and nums[left+lacc]%2==0:
+            lacc+=1
+        ret+=lacc*(idx-right)
+        return ret
--- 1968-Array-With-Elements-Not-Equal-to-Av.py
+++ 1968-Array-With-Elements-Not-Equal-to-Av.py
@@ -1,3 +1,15 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
-        
+        # beauty?
+        nums.sort()
+        ret=[]
+        idx0=0
+        idx1=len(nums)-1
+        for i in range(len(nums)):
+            if i%2:
+                ret.append(nums[idx1])
+                idx1-=1
+            else:
+                ret.append(nums[idx0])
+                idx0+=1
+        return ret
