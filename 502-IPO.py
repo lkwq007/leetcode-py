bbd4bf9e2f22709633f6f43cb230ee8bc148a407
lnyan <lkwq007@gmail.com>
2023-02-24 12:08:32 +0800

    Update

--- 502-IPO.py
+++ 502-IPO.py
@@ -1,3 +1,18 @@
 class Solution:
-    def findMaximizedCapital(self, k: int, W: int, Profits: List[int], Capital: List[int]) -> int:
-        
+    def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
+        cheap=[]
+        pheap=[(-p,c) for p,c in zip(profits,capital)]
+        import heapq
+        heapq.heapify(pheap)
+        while k>0 and pheap:
+            top=heapq.heappop(pheap)
+            p,c=-top[0],top[1]
+            if w>=c:
+                w+=p
+                k-=1
+            else:
+                heapq.heappush(cheap,(c,-p))
+            while cheap and cheap[0][0]<=w:
+                top=heapq.heappop(cheap)
+                heapq.heappush(pheap,(top[1],top[0]))
+        return w
