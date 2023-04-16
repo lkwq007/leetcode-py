3282eaef600aa885b518db0145fc67ab010bf6e6
lnyan <lkwq007@gmail.com>
2023-04-16 21:29:17 +0800

    Update

--- 1639-Number-of-Ways-to-Form-a-Target-Str.py
+++ 1639-Number-of-Ways-to-Form-a-Target-Str.py
@@ -1,16 +1,21 @@
 class Solution:
     def numWays(self, words: List[str], target: str) -> int:
-        lst=[0]*(len(words[0])+1)
-        prefix=[lst[:] for _ in range(26)]
+        total=len(words[0])
+        lst=[0]*total
+        tbl=[lst[:] for _ in range(26)]
         base=ord("a")
         for word in words:
-            for i in range(len(word)):
+            for i in range(total):
                 cur=ord(word[i])-base
-                prefix[cur][i]+=1
-        for i in range(len(prefix)):
-            for j in range(len(prefix[i])):
-                prefix[i][j]+=prefix[i][j-1]
+                tbl[cur][i]+=1
         term=10**9+7
-        import functools
-        total=len(words[0])
-        return probe(0,0)
+        last=[1]*(total+1)
+        dp=[0]*(total+1)
+        for i in range(len(target)):
+            cur=ord(target[i])-base
+            for j in range(total):
+                dp[j]=dp[j-1]+tbl[cur][j]*last[j-1]
+                dp[j]%=term
+            last=dp
+            dp=[0]*(total+1)
+        return last[-2]
